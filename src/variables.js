2f26304326dfd50795b89ea992f58cd0e2be65c5
Hassan Syyid <hassansyyid@gmail.com>
2021-07-16 17:07:23 -0400

    Get params at runtime instead of build time

--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,6 @@
-// DO NOT CHANGE THIS ONCE THE WEB APP HAS STARTED
-export const TENANT_ID = process.env["REACT_APP_TENANT_ID"];
-export const FLOW_ID = process.env["REACT_APP_FLOW_ID"];
-export const AWS_ENDPOINT = process.env["REACT_APP_AWS_ENDPOINT"];
+// params will be provided in search
+const params = (new URL(document.location)).searchParams;
+
+export const TENANT_ID = params.get("tenantId");
+export const FLOW_ID = params.get("flowId");
+export const AWS_ENDPOINT = params.get("awsEndpoint");
