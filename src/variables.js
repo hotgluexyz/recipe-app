936f813dba58d5b6befbdebffad46933920cca68
Hassan Syyid <hassansyyid@gmail.com>
2021-07-16 16:51:06 -0400

    Get variables from environment variables at build time

--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,4 @@
 // DO NOT CHANGE THIS ONCE THE WEB APP HAS STARTED
-export const TENANT_ID = "";
-export const FLOW_ID = "";
-export const AWS_ENDPOINT = "";
+export const TENANT_ID = process.env["REACT_APP_TENANT_ID"];
+export const FLOW_ID = process.env["REACT_APP_FLOW_ID"];
+export const AWS_ENDPOINT = process.env["REACT_APP_AWS_ENDPOINT"];
